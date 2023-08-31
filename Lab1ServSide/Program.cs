68a1987ddde0d94528dcc0f8aeb9b16e2c4a69bd
gqfri <gqfri@DESKTOP-HH7DQV5>
2023-08-31 12:50:36 -0400

    Add project files.

---
+++ Lab1ServSide/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Lab1ServSide
+{
+    internal class Program
+    {
+        private static List<VideoGame> games = new List<VideoGame>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new(@"C:\Users\gqfri\source\repos\Lab1ServSide\Lab1ServSide\videogames.csv");
+            string header = sr.ReadLine();
+            while (sr.ReadLine != null)
+            {
+                string fileread = sr.ReadLine();
+                if (fileread != null)
+                {
+                    games.Add(ProcessCSVLine(fileread));
+                }
+            }
+            foreach(VideoGame game in games)
+            {
+                Console.WriteLine(game.ToString());
+            }
+        }
+
+        static VideoGame ProcessCSVLine(string LineFromCSV)
+        {
+            string[] rawGameParts = LineFromCSV.Split(",");
+            List<string> cleanGameParts = new List<string>();
+            string cleanString = string.Empty;
+            for (int i = 0; i < rawGameParts.Length; i++)
+            {
+                cleanString += rawGameParts[i];
+                // more data processing / Cleaning?
+                cleanGameParts.Add(cleanString);
+                cleanString = "";
+            }
+            string Name = cleanGameParts[0];
+            string Platform = cleanGameParts[1];
+            string preparse1 = cleanGameParts[2];
+            int Year = Int32.Parse(preparse1);
+            string Genre = cleanGameParts[3];
+            string publisher = cleanGameParts[4];
+            string preParse2 = cleanGameParts[5];
+            double NASales = double.Parse(preParse2);
+            string preParse3 = cleanGameParts[6];
+            double EUSales = double.Parse(preParse3);
+            string preParse4 = cleanGameParts[7];
+            double JPSales = double.Parse(preParse4);
+            string preParse5 = cleanGameParts[8];
+            double OtherSales = double.Parse(preParse5);
+            string preparse6 = cleanGameParts[9];
+            double GlobalSales = double.Parse(preparse6);
+            VideoGame cleanGame = new VideoGame(Name, Platform, Year, Genre, publisher, NASales, EUSales, JPSales, OtherSales, GlobalSales);
+            return cleanGame;
+        }
+    }
+}
---
+++ Lab1ServSide/VideoGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1ServSide
+{
+    internal class VideoGame
+    {
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public double NASales { get; set; }
+        public double EUSales { get; set; }
+        public double JPSales { get; set; }
+        public double OtherSales { get; set; }
+        public double GlobalSales { get; set; }
+
+        public VideoGame(string name, string platform, int year, string genre, string publisher, double nASales, double eUSales, double jPSales, double otherSales, double globalSales)
+        {
+            Name = name;
+            Platform = platform;
+            Year = year;
+            Genre = genre;
+            Publisher = publisher;
+            NASales = nASales;
+            EUSales = eUSales;
+            JPSales = jPSales;
+            OtherSales = otherSales;
+            GlobalSales = globalSales;
+        }
+
+        public string toString()
+        {
+            string info = "";
+            info = (Name + Platform + Year + Genre + Publisher + NASales + EUSales + JPSales + OtherSales + GlobalSales);
+            return info;
+        }
+    }
+}
+
